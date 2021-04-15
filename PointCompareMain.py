ee43b4d0a5f5ec16adca3cf793de518bb96eda9c
hanjiawei <hanjiawei@cambricon.com>
2021-04-15 20:17:08 +0800

    init

--- PointCompareMain.py
+++ PointCompareMain.py
@@ -1,11 +1,25 @@
 import numpy as np
+import kdtree
 
 def reducePts(pts, dst):
     """
     reduces a point set, pts, in a stochastic manner, such that the minmum sdistance ibetween points is 'dst'.
     """
-    npoints = pts.shape[1]
-    in
+    nPoints = pts.shape[1]
+    indexSet = np.full((nPoints,1), True, dtype=bool)
+    RandOrd =  np.arange(nPoints)
+    np.random.shuffle(RandOrd)
+
+    # according to the points to create kd tree
+    NS = kdtree.create(np.transpose(pts))
+
+    # search the KTtree for close neighbours in a chunk-wise fashion to save memory if points cloud is really big
+    Chunks = np.arange(0,nPoints,min(4e06, nPoints-1))
+    for Chunk in range(len(Chunks)):
+        Range = np.arange(Chunks[Chunk],Chunks[Chunk + 1] + 1)
+        
+
+
 
 def PointCompareMain(cSet: int, Qdata: np.ndarray, dst: float, dataPath: str):
     # reduce points 0.2 mm neighbbourhood density
