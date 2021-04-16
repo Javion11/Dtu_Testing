512cf18b737b7b6eabdc0da7e12bd3e9df3851fa
hanjiawei <hanjiawei@cambricon.com>
2021-04-16 11:09:15 +0800

    init

--- PointCompareMain.py
+++ PointCompareMain.py
@@ -11,22 +11,29 @@ def reducePts(pts, dst):
     np.random.shuffle(RandOrd)
 
     # according to the points to create kd tree
+    pts_random = np.random.permutation(pts)
+    pts_random = list(pts_random)
     pts = list(pts)
     NS = kdtree.create(pts)
 
     # search the KTtree for close neighbours in a chunk-wise fashion to save memory if points cloud is really big
+    ptsOut = [] # save points to output
+
     Chunks = np.arange(0,nPoints,min(4e06, nPoints-1))
     for cChunk in range(len(Chunks)):
-        # Range = np.arange(Chunks[cChunk],Chunks[cChunk + 1] + 1)
-
-        """
-        Search the n nearest nodes of the given point which are within given distance
-        point must be a location, not a node. A list containing the n nearest nodes to the point within the distance will be returned.
-        """
-        pts_to_search = pts[Chunks[cChunk]:Chunks[cChunk + 1] + 1]
-        idx = NS.search_nn_dist(pts_to_search, dst) # the point inputing in the search is only one object
-
-        # for i in range()
+        pts_to_search = pts_random[Chunks[cChunk]:Chunks[cChunk + 1] + 1]
+        for pt_to_search in pts_to_search:
+            """
+            Search the n nearest nodes of the given point which are within given distance,point must be a location, not a node.
+            A list containing the n nearest nodes to the point within the distance will be returned.
+            Note:the point inputing in the search is only one object.
+            """
+            idx_points = NS.search_nn_dist(pt_to_search, dst) # the point inputing in the search is only one object
+            for idx_point in idx_points:
+                if idx_point not in ptsOut:
+                    ptsOut.append(idx_point)
+    print("downsample factor: {:.4f}".format(nPoints/len(ptsOut)))    
+    
 
 
 
