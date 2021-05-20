648b9638eb2bed91155e96c6cd11f9260eee5698
Javion11 <hanjiawei1111@outlook.com>
2021-05-20 16:45:54 +0800

    init

--- PointCompareMain.py
+++ PointCompareMain.py
@@ -28,7 +28,7 @@ def reducePts(pts: np.ndarray, dst: float) -> np.ndarray:
             pts_to_search = pts[RandOrd[Chunks[cChunk]:Chunks[cChunk + 1]], :]
         idx_points = NS.query_radius(pts_to_search, dst) # the point inputing in the search is only one object, return the index of pts(which create KDtree)
         
-        for idx_point, i in enumerate(idx_points):
+        for i, idx_point in enumerate(idx_points):
             id = RandOrd[i + Chunks[cChunk]]
             if indexSet[id]:
                 indexSet[idx_point] = False
