5ce9c6c6a7a7f00fe11fc361c07856cbcf4776b8
Javion11 <hanjiawei1111@outlook.com>
2021-06-01 20:53:51 +0800

    compare psnr and ssim

--- plyread.py
+++ plyread.py
@@ -12,9 +12,3 @@ def plyread(file_dir: str) -> np.array:
         vertex_y = plydata['vertex']['y']
         vertex_z = plydata['vertex']['z']
     return [vertex_x,vertex_y,vertex_z]
-
-# test code, please ignore it
-if __name__ == "__main__":
-    plydata = PlyData.read("/algo/algo/hanjiawei/mvs_result/baseline_traineval_noise/baseline_noise4/mvsnet118_l3.ply")
-    vertex_x = plydata['vertex']['x']
-    print(len(vertex_x))
--- psnr_ssim/util_psnr_ssim.py
+++ psnr_ssim/util_psnr_ssim.py
@@ -1,4 +1,6 @@
+import re
 from math import exp
+import numpy as np
 import torch
 import torch.nn.functional as F
 
@@ -57,3 +59,45 @@ def ssim(img1, img2, window_size=11, size_average=True, full=False):
     if full:
         return ret, cs
     return ret
+
+
+def read_pfm(filename):
+    file = open(filename, 'rb')
+    color = None
+    width = None
+    height = None
+    scale = None
+    endian = None
+
+    header = file.readline().decode('utf-8').rstrip()
+    if header == 'PF':
+        color = True
+    elif header == 'Pf':
+        color = False
+    else:
+        raise Exception('Not a PFM file.')
+
+    dim_match = re.match(r'^(\d+)\s(\d+)\s$', file.readline().decode('utf-8'))
+    if dim_match:
+        width, height = map(int, dim_match.groups())
+    else:
+        raise Exception('Malformed PFM header.')
+
+    scale = float(file.readline().rstrip())
+    if scale < 0:  # little-endian
+        endian = '<'
+        scale = -scale
+    else:
+        endian = '>'  # big-endian
+
+    data = np.fromfile(file, endian + 'f')
+    shape = (height, width, 3) if color else (height, width)
+
+    data = np.reshape(data, shape)
+    data = np.flipud(data)
+    file.close()
+    return data, scale
+
+confidence = read_pfm("/algo/algo/hanjiawei/denoise_result/mvsnet_denoise_clean_newunet_newfeaturenet_l1_lossweight_dynamic_method3/scan1/confidence/00000000.pfm")[0]
+photo_mask = confidence > 0.8
+print(photo_mask)
